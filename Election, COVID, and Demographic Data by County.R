e03c4174861ef2e46ab4db0723950a4704cfa804
FrancescoSart <94694197+FrancescoSart@users.noreply.github.com>
2022-05-27 12:00:43 +0200

    Update Election, COVID, and Demographic Data by County.R

--- Election, COVID, and Demographic Data by County.R
+++ Election, COVID, and Demographic Data by County.R
@@ -18,6 +18,60 @@ library(cowplot)
 library(statebins)
 library(DT)
 
+
+library(gRbase)
+library(ISLR2)
+library(leaps)
+
+v<-c(cou2$percentage20_Donald_Trump<0.5)
+regfit.full <- regsubsets(v~., data=cou2[,-1:-13], really.big = T,nvmax = 38)
+reg.summary <- summary(regfit.full)
+reg.summary$outmat
+reg.summary$rsq
+par(mfrow=c(2,2))
+plot(reg.summary$rss,xlab="Number of Variables",ylab="RSS",type="l")
+plot(reg.summary$adjr2,xlab="Number of Variables",ylab="Adjusted Rsq",type="l")
+which.max(reg.summary$adjr2)
+points(11,reg.summary$adjr2[11], col="red",cex=2,pch=20)
+
+
+# Mallow's Cp with its smallest value
+plot(reg.summary$cp,xlab="Number of Variables",ylab="Cp",type='l')
+which.min(reg.summary$cp)
+points(10,reg.summary$cp[10],col="red",cex=2,pch=20)
+
+# BIC with its smallest value
+plot(reg.summary$bic,xlab="Number of Variables",ylab="BIC",type='l')
+which.min(reg.summary$bic)
+points(6,reg.summary$bic[6],col="red",cex=2,pch=20)
+
+par(mfrow=c(1,1))
+
+
+par(mfrow=c(2,2))
+plot(regfit.full,scale="r2")
+plot(regfit.full,scale="adjr2")
+plot(regfit.full,scale="Cp")
+
+plot(regfit.full,scale="bic")
+coef(regfit.full,6)
+par(mfrow=c(1,1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 options(repr.plot.width = 17, repr.plot.height = 9)
 
 # US MAP 
