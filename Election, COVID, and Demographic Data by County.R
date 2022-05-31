812c17fa82810671b61ddfaf807006ffc761bb87
FrancescoSart <94694197+FrancescoSart@users.noreply.github.com>
2022-05-31 11:18:33 +0200

    Update Election, COVID, and Demographic Data by County.R

--- Election, COVID, and Demographic Data by County.R
+++ Election, COVID, and Demographic Data by County.R
@@ -4,8 +4,8 @@ data <- na.omit(data_uncleaned)
 attach(data)
 
 rownames(data) <- 1:nrow(data)
-plot(percentage20_Donald_Trump, lat)
-hist(lat[votes20_Donald_Trump], freq = FALSE)
+plot(percentage20_Donald_Trump, Hispanic)
+hist(lat, freq = FALSE)
 lines(density(na.omit(lat[votes20_Donald_Trump])))
 
 library(tidyverse)
@@ -117,9 +117,44 @@ p<-ggplot(data = usa_election, aes(x = long, y = lat),color = usa_election$Party
 p
 
 
+abb <- data.frame(state = state.name, Abb = state.abb)
+abb2<-abb
+abb2$state<-tolower(abb2$state)
+election2020 <- data %>% 
+  group_by(state) %>% 
+  summarise(votes20_Joe_Biden = sum(votes20_Joe_Biden),
+            votes20_Donald_Trump = sum(votes20_Donald_Trump)) %>% 
+  ungroup() %>% 
+  gather(Candidate, Votes, -state) %>% 
+  mutate(Party = factor(if_else(Candidate == "votes20_Joe_Biden", "Democrat", "Republican"))) %>% 
+  left_join(abb2) %>% 
+  #mutate(state = tolower(state)) %>% 
+  rename("region" = state)
+
+election22020 <- election2020 %>% 
+  group_by(region, Abb) %>% 
+  summarise(Votes = max(Votes)) %>% 
+  ungroup() %>% 
+  inner_join(election2020)
 
 
 
+
+election22020<-election22020[,-2]
+
+colnames(abb2)[1]<-"region"
+colnames(election22020)[1]<-"Abb"
+election32020<-left_join(election22020,abb2)
+# Left Join for creating map
+usa_election2020 <- left_join(usa, election32020) %>% 
+  filter(!is.na(Party))
+
+
+# First Map
+p2020<-ggplot(data = usa_election2020, aes(x = long, y = lat),color = usa_election2020$Party)+
+  geom_polygon(aes(group = group, fill = Party),color = "gray90", size = 0.1) +
+  coord_map(projection = "albers", lat0 = 39, lat1 = 45) + scale_fill_manual(values = alpha(c("blue2", "red3"), 1))
+p2020
 
 
 
@@ -148,45 +183,25 @@ usa_data <- left_join(usa, data3)
 # First Map
 po<-ggplot(data = usa_data, aes(x = long, y = lat),color = usa_data$TotalPop)+
   geom_polygon(aes(group = group, fill = TotalPop),color = "gray90", size = 0.1) +
-  coord_map(projection = "albers", lat0 = 39, lat1 = 45) 
-
-
-po2=po+scale_fill_gradient(names<-"TotalPop", low="White", high = "deepskyblue4")
-
-po2
+  coord_map(projection = "albers", lat0 = 39, lat1 = 45) +scale_fill_gradient(names<-"TotalPop", low="White", high = "deepskyblue4")
+po
 
 
 b<-ggplot(data = usa_data, aes(x = long, y = lat),color = usa_data$Black)+
   geom_polygon(aes(group = group, fill = Black),color = "gray90", size = 0.1) +
-  coord_map(projection = "albers", lat0 = 39, lat1 = 45) 
-
-
-b2=b+scale_fill_gradient(names<-"Black", low="White", high = "Black")
-
-b2
+  coord_map(projection = "albers", lat0 = 39, lat1 = 45) +scale_fill_gradient(names<-"Black", low="White", high = "Black")
+b
 
 h<-ggplot(data = usa_data, aes(x = long, y = lat),color = usa_data$Hispanic)+
   geom_polygon(aes(group = group, fill = Hispanic),color = "gray90", size = 0.1) +
-  coord_map(projection = "albers", lat0 = 39, lat1 = 45) 
+  coord_map(projection = "albers", lat0 = 39, lat1 = 45) +scale_fill_gradient(names<-"Hispanic", low="White", high = "coral3")
+h
 
-
-h2=h+scale_fill_gradient(names<-"Hispanic", low="White", high = "coral3")
-
-h2
 
 a<-ggplot(data = usa_data, aes(x = long, y = lat),color = usa_data$Asian)+
   geom_polygon(aes(group = group, fill = Asian),color = "gray90", size = 0.1) +
-  coord_map(projection = "albers", lat0 = 39, lat1 = 45) 
-
-
-a2=a+scale_fill_gradient(names<-"Asian", low="White", high = "darkgoldenrod")
-
-a2
-
-
-
-
-
+  coord_map(projection = "albers", lat0 = 39, lat1 = 45) +scale_fill_gradient(names<-"Asian", low="White", high = "darkgoldenrod")
+a
 
 
 
@@ -198,3 +213,36 @@ q=c(percentage20_Joe_Biden>0.5)
 
 ggplot(data,aes(x=data$Asian, group=q,fill=q))+
   geom_histogram(position="identity",alpha=0.5,binwidth=1)+theme_bw()
+
+
+S<-cov(data[,4:51])
+P<-cov2cor(S)
+
+
+library(corrplot)
+
+corrplot(P)
+
+
+library(glmnet)
+
+X <- model.matrix(percentage20_Joe_Biden~.,data)
+X<-X[,-1]
+y<-percentage20_Joe_Biden
+grid<-10^seq(10,-1,length=100)
+ridge.mod<-glmnet(X,y,alpha = 0,lambda = grid)
+plot(ridge.mod)
+
+
+train <- sample(1:nrow(X),nrow(X)/2)
+test<-(-train)
+y.test<-y[test]
+ridge.pred<-predict(ridge.mod,s=4,newx = X[test,],type = "response")
+mean((ridge.pred-y.test)^2)
+
+
+lm(percentage20_Joe_Biden~.,data[,4:51],subset = train)
+predict(ridge.mod,s=0,exact = TRUE,type = "coefficients",x=X[train,],y=y[train])[1:20,]
+
+
+
